806b9527eef8748a7aa43812d3096d720c730c16
FringeSource <cedric.vigneau@live.ca>
2020-04-11 22:50:40 -0400

    Projet final

--- Programmation1/Examen2 JavaScript Photociti 2/js/script.js
+++ Programmation1/Examen2 JavaScript Photociti 2/js/script.js
@@ -4,14 +4,6 @@
 
 let motValide = false;
 
-$('#L5').hide();
-$('#L6').hide();
-$('#L7').hide();
-$('#L8').hide();
-$('#L9').hide();
-$('#L10').hide();
-$('#L11').hide();
-
 function ContientLettresSeulement(str) {
     return (/^[A-Za-z*=]+$/.test(str));
 }
@@ -88,4 +80,29 @@ $('#fondRose').click(function() {
 
 $('#fondRouge').click(function() {
     $('.rowBg').css('background-image', 'url("background/rouge.jpg")');
+});
+
+$(document).on('click', '.img-fluid', function() {
+    $('#carousel').modal('toggle');
+    if()
+});
+
+$('#carousel').on('slide.bs.carousel', function (e) {
+
+    let $e = $(e.relatedTarget);
+    let idx = $e.index();
+    let itemsPerSlide = 3;
+    let totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        let it = itemsPerSlide - (totalItems - idx);
+        for (let i=0; i<it; i++) {
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
 });
